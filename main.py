0020087f5541e6b86ea88de652b3bf0b7d02b47a
azamat4205-wq <azamat4205@gmail.com>
2026-06-25 23:44:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает!")
 
-TOKEN = os.getenv("8883791568:AAG3ZB0W3NYxwmI2hgFEUVopWS6uukHTZ3k")
+TOKEN = os.getenv("TOKEN")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
