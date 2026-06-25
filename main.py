5630dfb98b9be2a057a45215e04fec2e2a777387
azamat4205-wq <azamat4205@gmail.com>
2026-06-25 23:55:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "НОВЫЙ_ТОКЕН_ОТ_BOTFATHER"
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает!")
 
 app = Application.builder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
