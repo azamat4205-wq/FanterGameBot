9dc615f4b80002a48e1bce0bd3c6089909e8ae73
azamat4205-wq <azamat4205@gmail.com>
2026-06-25 22:59:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,12 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8915110177:AAE14FZDUgJsXGuyf_C5Z-dJO6Vj0OQcfPI")
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Бот работает.")
+    await update.message.reply_text("Бот работает!")
 
-def main():
-    if not TOKEN:
-        raise ValueError("Переменная TOKEN не найдена!")
+TOKEN = os.getenv("TOKEN")
 
-    app = Application.builder().token(TOKEN).build()
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
-    app.add_handler(CommandHandler("start", start))
-
-    print("Бот запущен...")
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
+app.run_polling()
