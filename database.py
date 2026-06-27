3a5dea271ea944c9174705f957fd607608b743e2
azamat4205-wq <azamat4205@gmail.com>
2026-06-27 11:40:42 +0300

    Update database.py

--- database.py
+++ database.py
@@ -5,13 +5,14 @@ cursor = db.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
-    user_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     username TEXT,
     coins INTEGER DEFAULT 100,
     wins INTEGER DEFAULT 0,
     loses INTEGER DEFAULT 0,
     cases INTEGER DEFAULT 0,
-    frame TEXT DEFAULT 'Нет'
+    frame TEXT DEFAULT 'Нет',
+    multiplier INTEGER DEFAULT 1
 )
 """)
 
@@ -20,22 +21,63 @@ db.commit()
 
 def add_user(user_id, username):
     cursor.execute(
-        "INSERT OR IGNORE INTO users(user_id, username) VALUES(?, ?)",
+        "INSERT OR IGNORE INTO users(id, username) VALUES(?, ?)",
         (user_id, username)
     )
     db.commit()
 
 
 def get_user(user_id):
-    cursor.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
+    cursor.execute(
+        "SELECT * FROM users WHERE id=?",
+        (user_id,)
+    )
     return cursor.fetchone()
 
-def get_user(user_id):
-    conn = sqlite3.connect("database.db")
-    cur = conn.cursor()
 
-    cur.execute("SELECT * FROM users WHERE id = ?", (user_id,))
-    user = cur.fetchone()
+def add_coins(user_id, coins):
+    cursor.execute(
+        "UPDATE users SET coins = coins + ? WHERE id=?",
+        (coins, user_id)
+    )
+    db.commit()
 
-    conn.close()
-    return user
+
+def remove_coins(user_id, coins):
+    cursor.execute(
+        "UPDATE users SET coins = coins - ? WHERE id=?",
+        (coins, user_id)
+    )
+    db.commit()
+
+
+def add_win(user_id):
+    cursor.execute(
+        "UPDATE users SET wins = wins + 1 WHERE id=?",
+        (user_id,)
+    )
+    db.commit()
+
+
+def add_lose(user_id):
+    cursor.execute(
+        "UPDATE users SET loses = loses + 1 WHERE id=?",
+        (user_id,)
+    )
+    db.commit()
+
+
+def add_case(user_id):
+    cursor.execute(
+        "UPDATE users SET cases = cases + 1 WHERE id=?",
+        (user_id,)
+    )
+    db.commit()
+
+
+def set_frame(user_id, frame):
+    cursor.execute(
+        "UPDATE users SET frame=? WHERE id=?",
+        (frame, user_id)
+    )
+    db.commit()
