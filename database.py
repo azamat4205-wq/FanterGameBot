6f2b567c86cf3d6c62f23b1c1ef7564faf10c5ab
azamat4205-wq <azamat4205@gmail.com>
2026-06-27 10:51:13 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,33 +1,31 @@
 import sqlite3
 
-db = sqlite3.connect("players.db", check_same_thread=False)
+db = sqlite3.connect("database.db", check_same_thread=False)
 cursor = db.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS players(
+CREATE TABLE IF NOT EXISTS users(
     user_id INTEGER PRIMARY KEY,
     username TEXT,
     coins INTEGER DEFAULT 100,
     wins INTEGER DEFAULT 0,
-    loses INTEGER DEFAULT 0
+    loses INTEGER DEFAULT 0,
+    cases INTEGER DEFAULT 0,
+    frame TEXT DEFAULT 'Нет'
 )
 """)
 
 db.commit()
 
 
-def register(user):
+def add_user(user_id, username):
     cursor.execute(
-        "SELECT user_id FROM players WHERE user_id=?",
-        (user.id,)
+        "INSERT OR IGNORE INTO users(user_id, username) VALUES(?, ?)",
+        (user_id, username)
     )
+    db.commit()
 
-    if cursor.fetchone() is None:
-        cursor.execute(
-            "INSERT INTO players(user_id, username) VALUES(?,?)",
-            (
-                user.id,
-                user.first_name
-            )
-        )
-        db.commit()
+
+def get_user(user_id):
+    cursor.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
+    return cursor.fetchone()
