7c7eaa888ef3f8e30c91d8ff24f8981debcf308b
azamat4205-wq <azamat4205@gmail.com>
2026-06-28 16:09:02 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://ТВОЙ_PROJECT.railway.app")
-PORT = int(os.getenv("PORT", 8000))
-ADMIN_IDS = [7795629827]  # Твой ID для админ-функций
-DB_PATH = "bot.db"
