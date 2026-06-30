cf2d97843c1aaa2c337f88ebd1e7df52cd8d21a3
azamat4205-wq <azamat4205@gmail.com>
2026-06-30 10:40:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-BOT_USERNAME = os.getenv("BOT_USERNAME")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не найден!")
